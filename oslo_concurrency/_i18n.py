343e3bd0e16df1106d82fa6087a7247dc67bb52b
Ian Cordasco <ian.cordasco@rackspace.com>
2015-01-30 18:39:15 -0600

    Drop use of namespaced oslo.i18n
    Related-blueprint: drop-namespace-packages
    
    Change-Id: Ic8247cb896ba6337932d7a74618debd698584fa0

--- oslo_concurrency/_i18n.py
+++ oslo_concurrency/_i18n.py
@@ -14,9 +14,9 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from oslo import i18n
+import oslo_i18n
 
-_translators = i18n.TranslatorFactory(domain='oslo.concurrency')
+_translators = oslo_i18n.TranslatorFactory(domain='oslo.concurrency')
 
 # The primary translation function using the well-known name "_"
 _ = _translators.primary
