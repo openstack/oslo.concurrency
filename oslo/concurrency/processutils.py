397729bca2e58433d4dbe09a3fba51887b2d0966
Daniel P. Berrange <berrange@redhat.com>
2015-07-02 14:49:12 +0100

    processutils: ensure on_completion callback is always called
    If the subprocess.Popen.communicate method raises an exception,
    the on_completion callback is never invoked. If a caller is
    trying to use on_execute + on_completion to track lifecycle
    of a process this creates a problem, as they cannot reliably
    detect completion.
    
    Conflicts:
            oslo/concurrency/processutils.py
            tests/unit/test_processutils.py
    
    Change-Id: I22b2d7bde8797276f7670bc289d915dab5122481
    Closes-bug: #1470868
    (cherry picked from commit ab78480659834ed07c84f8569df5d2c27965af00)

--- oslo/concurrency/processutils.py
+++ oslo/concurrency/processutils.py
@@ -223,16 +223,17 @@ def execute(*cmd, **kwargs):
             if on_execute:
                 on_execute(obj)
 
-            result = obj.communicate(process_input)
+            try:
+                result = obj.communicate(process_input)
 
-            obj.stdin.close()  # pylint: disable=E1101
-            _returncode = obj.returncode  # pylint: disable=E1101
-            end_time = time.time() - start_time
-            LOG.log(loglevel, 'CMD "%s" returned: %s in %ss' %
-                    (sanitized_cmd, _returncode, end_time))
-
-            if on_completion:
-                on_completion(obj)
+                obj.stdin.close()  # pylint: disable=E1101
+                _returncode = obj.returncode  # pylint: disable=E1101
+                end_time = time.time() - start_time
+                LOG.log(loglevel, 'CMD "%s" returned: %s in %ss' %
+                        (sanitized_cmd, _returncode, end_time))
+            finally:
+                if on_completion:
+                    on_completion(obj)
 
             if not ignore_exit_code and _returncode not in check_exit_code:
                 (stdout, stderr) = result
