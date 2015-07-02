2855b6e66256f7189c2298b2968e831480443ff2
Daniel P. Berrange <berrange@redhat.com>
2015-07-02 14:49:12 +0100

    processutils: ensure on_completion callback is always called
    If the subprocess.Popen.communicate method raises an exception,
    the on_completion callback is never invoked. If a caller is
    trying to use on_execute + on_completion to track lifecycle
    of a process this creates a problem, as they cannot reliably
    detect completion.
    
    Conflicts:
            oslo_concurrency/processutils.py
            oslo_concurrency/tests/unit/test_processutils.py
    
    Change-Id: I22b2d7bde8797276f7670bc289d915dab5122481
    Closes-bug: #1470868
    (cherry picked from commit ab78480659834ed07c84f8569df5d2c27965af00)

--- oslo_concurrency/tests/unit/test_processutils.py
+++ oslo_concurrency/tests/unit/test_processutils.py
@@ -73,6 +73,24 @@ class UtilsTest(test_base.BaseTestCase):
         self.assertEqual(1, on_execute_callback.call_count)
         self.assertEqual(1, on_completion_callback.call_count)
 
+    @mock.patch.object(subprocess.Popen, "communicate")
+    def test_execute_with_callback_and_errors(self, mock_comm):
+        on_execute_callback = mock.Mock()
+        on_completion_callback = mock.Mock()
+
+        def fake_communicate(*args):
+            raise IOError("Broken pipe")
+
+        mock_comm.side_effect = fake_communicate
+
+        self.assertRaises(IOError,
+                          processutils.execute,
+                          "/bin/true",
+                          on_execute=on_execute_callback,
+                          on_completion=on_completion_callback)
+        self.assertEqual(1, on_execute_callback.call_count)
+        self.assertEqual(1, on_completion_callback.call_count)
+
     def test_execute_with_preexec_fn(self):
         # NOTE(dims): preexec_fn is set to a callable object, this object
         # will be called in the child process just before the child is
